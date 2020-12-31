8a070587692f00dbb6f403e3497c50af2badaed2
MDC <markdcross@gmail.com>
2020-12-31 17:23:29 -0500

    Added backend server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send({ msg: 'Welcome to Nucleus' }));
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
