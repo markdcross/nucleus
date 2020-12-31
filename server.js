ee5cb66442f4b97261038437dcdde7c5bb5d3ef4
MDC <markdcross@gmail.com>
2020-12-31 17:42:29 -0500

    Connected backend and server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
